13443a6b5b19fcfb091c1802e45849572b209fe0
acientsquirrel <hanbong34@gmail.com>
2025-10-25 18:28:59 +0900

    reset_world() drw -> render

--- full_it_up_main.py
+++ full_it_up_main.py
@@ -2,8 +2,12 @@ from pico2d import *
 
 
 
+
 open_canvas()
 running = True
+
+def reset_world():
+    pass
 
 
 def handle_events():
@@ -14,14 +18,17 @@ def update_world():
     pass
 
 
-def draw_world():
+def render_world():
     pass
 
+
+
+reset_world()
 
 while running:
     handle_events()
     update_world()
-    draw_world()
+    render_world()
 
     delay(0.05)
 close_canvas()
