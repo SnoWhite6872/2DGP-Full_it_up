332fea894593b46c62e8023a12f4e39aa8c260b9
SnoWhite6872 <144667826+SnoWhite6872@users.noreply.github.com>
2025-10-26 18:15:24 +0900

    run행동 클래스 생성

--- chabear.py
+++ chabear.py
@@ -1,15 +1,32 @@
 from pico2d import load_image
 
 
+class Run:
+    def __init__(self, chabear):
+        self.chabear = chabear
+    def enter(self,e):
+        pass
+    def exit(self,e):
+        pass
+    def do(self):
+        pass
+    def draw(self):
+        self.chabear.image.draw(self.chabear.x, self.chabear.y)
+
+
+
+
 class Chabear:
     def __init__(self):
         self.image = load_image('Cha_bear.png')
+        self.x, self.y = 252, 525
+        self.Run = Run(self)
         pass
     def draw(self):
-        self.image.draw(252, 300)
         pass
     def update(self):
         pass
 
     def handle_event(self, event):
+        #self.Run
         pass
