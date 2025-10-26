da04176a2777b67478150a749b302f3cb8503304
SnoWhite6872 <144667826+SnoWhite6872@users.noreply.github.com>
2025-10-26 17:54:15 +0900

    handle event. 종료 방식

--- full_it_up_main.py
+++ full_it_up_main.py
@@ -9,6 +9,13 @@ running = True
 
 def handle_events():
     global running
+
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            running = False
     pass
 
 def reset_world():
