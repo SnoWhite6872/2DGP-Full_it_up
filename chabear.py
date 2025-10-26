987bc9c4402ebbb7ac38c20cd6db3ff0a3508df3
SnoWhite6872 <144667826+SnoWhite6872@users.noreply.github.com>
2025-10-26 18:49:45 +0900

    키 입력에 대한 코드는 실행이 되는데 좌우 이동에 문제가 있음 - 오류

--- chabear.py
+++ chabear.py
@@ -1,14 +1,25 @@
 from pico2d import load_image
-
+from sdl2 import SDL_KEYUP, SDL_KEYDOWN, SDLK_RIGHT, SDLK_LEFT
 
 class Run:
     def __init__(self, chabear):
         self.chabear = chabear
     def enter(self,e):
+        if e.type == SDL_KEYDOWN:
+            if e.key == SDLK_RIGHT:
+                self.chabear.dir =1
+            elif e.key == SDLK_LEFT:
+                self.chabear.dir = -1
+        elif e.type == SDL_KEYUP:
+            if e.key == SDLK_RIGHT:
+                self.chabear.dir = -1
+            elif e.key == SDLK_LEFT:
+                self.chabear.dir = 1
         pass
     def exit(self,e):
         pass
     def do(self):
+        self.chabear.x += self.chabear.dir *10
         pass
     def draw(self):
         self.chabear.image.draw(self.chabear.x, self.chabear.y)
@@ -20,15 +31,21 @@ class Chabear:
     def __init__(self):
         self.image = load_image('Cha_bear.png')
         self.x, self.y = 252, 525
+        self.dir = 0
+
+
         self.Run = Run(self)
         pass
-    def draw(self):
-        self.Run.draw()
-        pass
+
     def update(self):
         self.Run.do()
         pass
 
-    def handle_event(self, event):
+    def draw(self):
+        self.Run.draw()
+        pass
 
+
+    def handle_event(self, event):
+        self.Run.enter(event)
         pass
