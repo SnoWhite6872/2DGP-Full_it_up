d97130ea473bb4237deda4c9f1b668d3553b2df5
acientsquirrel <hanbong34@gmail.com>
2025-10-27 10:40:00 +0900

    상하 움직임 구현 - 키 2개를 동시에 눌렸을 떄 문제 있음

--- chabear.py
+++ chabear.py
@@ -1,5 +1,5 @@
 from pico2d import load_image
-from sdl2 import SDL_KEYUP, SDL_KEYDOWN, SDLK_RIGHT, SDLK_LEFT
+from sdl2 import SDL_KEYUP, SDL_KEYDOWN, SDLK_RIGHT, SDLK_LEFT, SDLK_UP, SDLK_DOWN
 
 class Run:
     def __init__(self, chabear):
@@ -7,18 +7,25 @@ class Run:
     def enter(self,e):
         if e.type == SDL_KEYDOWN:
             if e.key == SDLK_RIGHT:
-                self.chabear.dir =1
+                self.chabear.w_dir =1
             elif e.key == SDLK_LEFT:
-                self.chabear.dir = -1
+                self.chabear.w_dir = -1
+            elif e.key == SDLK_UP:
+                self.chabear.h_dir = 1
+            elif e.key == SDLK_DOWN:
+                self.chabear.h_dir = -1
         elif e.type == SDL_KEYUP:
             if e.key == SDLK_RIGHT or e.key == SDLK_LEFT:
-                self.chabear.dir =0   #딕셔너리로 상태 변경시 수정 필요
+                self.chabear.w_dir =0   #딕셔너리로 상태 변경시 수정 필요
+            elif e.key == SDLK_UP or e.key ==SDLK_DOWN:
+                self.chabear.h_dir =0
 
         pass
     def exit(self,e):
         pass
     def do(self):
-        self.chabear.x += self.chabear.dir *5
+        self.chabear.x += self.chabear.w_dir *5
+        self.chabear.y += self.chabear.h_dir * 5
         pass
     def draw(self):
         self.chabear.image.draw(self.chabear.x, self.chabear.y)
@@ -30,8 +37,8 @@ class Chabear:
     def __init__(self):
         self.image = load_image('Cha_bear.png')
         self.x, self.y = 252, 525
-        self.dir = 0
-
+        self.w_dir = 0
+        self.h_dir = 0
 
         self.Run = Run(self)
         pass
