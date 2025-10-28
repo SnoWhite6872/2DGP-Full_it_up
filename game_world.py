7470368c3d30f3c649badfb98055e95c6d281ae7
SnoWhite6872 <hanbong34@gmail.com>
2025-10-28 23:26:31 +0900

    객체 추가를 위한 새로운 소스 파일

---
+++ game_world.py
@@ -0,0 +1,15 @@
+world = []
+
+def add_object(o):
+    world.append(o)
+
+def update():
+    for o in world:
+        o.update()
+
+def render():
+    for o in world:
+        o.draw()
+
+def remove_object(o):
+    world.remove(o)
