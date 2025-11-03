176ba942ac4176b56ff9726aedfbad942b2e80a9
SnoWhite6872 <hanbong34@gmail.com>
2025-11-04 00:53:57 +0900

    run함수 생성

---
+++ game_framework.py
@@ -0,0 +1,19 @@
+running = None
+stack = None
+
+
+def run(start_mode):
+    global stack, running
+
+    running = True
+    stack = [start_mode]
+    start_mode.init()
+
+    while running:     #STACK에 있는 모드 실행
+        stack[-1].handle_events()
+        stack[-1].update()
+        stack[-1].draw()
+
+    while (len(stack) > 0):  #STACK에 모드가 남아 있다면 제거
+        stack[-1].finish()
+        stack.pop()
