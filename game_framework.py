7414daaeb8dc0272424b71c7fd35732a11b4c9a7
SnoWhite6872 <hanbong34@gmail.com>
2025-11-04 00:55:09 +0900

    change, push, pop모드 선언

--- game_framework.py
+++ game_framework.py
@@ -1,6 +1,23 @@
 running = None
 stack = None
 
+def change_mode(mode):
+    pass
+
+def push_mode(mode):
+    pass
+
+def pop_mode():
+    pass
+
+
+
+
+def quit():
+    global running
+    running = False
+
+
 
 def run(start_mode):
     global stack, running
