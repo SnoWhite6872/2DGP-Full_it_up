bc819f05891fc0a8064b5cbb856aa40ecd26d9f8
SnoWhite6872 <hanbong34@gmail.com>
2025-11-10 23:36:31 +0900

    충돌 관리 딕셔너리 collision_pair
    충돌 그룹 추가하기

--- game_world.py
+++ game_world.py
@@ -24,3 +24,16 @@ def remove_object(o):
 def clear():
     for layer in world:
         layer.clear()
+
+
+def collide(a,b): #충돌 체크 함수 a와 b의 충돌
+    left_a, bottom_a, right_a, top_a = a.get_bb() #상하좌우 끝의 범위 전달
+    left_b, bottom_b, right_b, top_b = b.get_bb()
+
+    if left_a > right_b: return False
+    if right_a < left_b: return False
+    if top_a < bottom_b: return False
+    if bottom_a > top_b: return False
+
+    return True
+
