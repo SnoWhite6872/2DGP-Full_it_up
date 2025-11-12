2a4e4ff6e1d81be6ae6ce7b5a4c93c0cba102671
SnoWhite6872 <hanbong34@gmail.com>
2025-11-12 19:31:34 +0900

    배경 크기 조절

--- state_machine.py
+++ state_machine.py
@@ -16,13 +16,14 @@ class StateMachine:
     def handle_state_event(self, state_event):
         if state_event[1] == SDLK_q:
             pass
-        for check_event in self.rules[self.cur_state].keys():
-            if check_event(state_event):
-                self.next_state = self.rules[self.cur_state][check_event]
-                self.cur_state.exit(state_event)
-                self.next_state.enter(state_event)
-                print(f'{self.cur_state.__class__.__name__} -----------{event_to_string(state_event)}----------> {self.next_state.__class__.__name__}')
-                self.cur_state = self.next_state
-                self.prev_state = self.cur_state
-                return
+        else:
+            for check_event in self.rules[self.cur_state].keys():
+                if check_event(state_event):
+                    self.next_state = self.rules[self.cur_state][check_event]
+                    self.cur_state.exit(state_event)
+                    self.next_state.enter(state_event)
+                    print(f'{self.cur_state.__class__.__name__} -----------{event_to_string(state_event)}----------> {self.next_state.__class__.__name__}')
+                    self.cur_state = self.next_state
+                    self.prev_state = self.cur_state
+                    return
         print(f'처리되지 않은 이벤트{event_to_string(state_event)}가 있습니다.')
