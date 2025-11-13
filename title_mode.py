2549340199bc5ae18e66be263708cefad27e9214
SnoWhite6872 <hanbong34@gmail.com>
2025-11-14 00:31:14 +0900

    title 파일 기본적인 함수

--- title_mode.py
+++ title_mode.py
@@ -4,3 +4,19 @@ import full_it_up_main
 
 image = None
 
+def init():
+    global image
+    image = load_image('spare_title')
+
+def finish():
+    pass
+
+
+def update():
+    pass
+
+def draw():
+    pass
+
+def handle_events():
+    pass
