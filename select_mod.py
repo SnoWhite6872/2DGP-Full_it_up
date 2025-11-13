802af74b617cf3f24f0105bcbe664802f2dd6d41
SnoWhite6872 <hanbong34@gmail.com>
2025-11-14 01:59:09 +0900

    기본 함수 생성

---
+++ select_mod.py
@@ -0,0 +1,22 @@
+from pico2d import *
+import game_framework
+import full_it_up_main
+
+image0 = None
+image1 = None
+image2 = None
+
+def init():
+    pass
+
+def finish():
+    pass
+
+def update():
+    pass
+
+def draw():
+    pass
+
+def handle_events():
+    pass
