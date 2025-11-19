53343fc49132eac777803b668c84d615f7ff6ca8
SnoWhite6872 <hanbong34@gmail.com>
2025-11-20 03:00:22 +0900

    범위를 넘어가면 쿠키 제거

--- cookie.py
+++ cookie.py
@@ -20,6 +20,9 @@ class Cookie:
     def update(self):
         self.x += self.speed * game_framework.frame_time * PIXEL_PER_METER
 
+        if self.x <0 or self.x > 1480:
+            game_world.remove_object(self)
+
     def get_bb(self):
         return self.x - 25, self.y - 25, self.x + 25, self.y + 25
 
