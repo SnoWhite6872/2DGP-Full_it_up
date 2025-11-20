6030f8d2c139379a364242b0ff4e80d7e3872c7e
SnoWhite6872 <hanbong34@gmail.com>
2025-11-20 16:39:50 +0900

    아이템 구현

---
+++ item.py
@@ -0,0 +1,35 @@
+from pico2d import *
+import game_framework
+import game_world
+import random
+
+PIXEL_PER_METER = (1.0 / 0.03)
+
+class Icetea:
+    image = None
+
+    def __init__(self, x=random.randint(0, 1400), y=random.randint(0, 1000)):
+        if Icetea.image == None:
+            Icetea.image = load_image('I_Ice01.png')
+        self.x = x
+        self.y = y
+
+
+    def draw(self):
+        self.image.draw(self.x, self.y, 50, 50)
+        draw_rectangle(*self.get_bb())
+
+    def update(self):
+        # self.x += self.speed * game_framework.frame_time * PIXEL_PER_METER
+        #
+        # if self.x <0 or self.x > 1480:
+        #     game_world.remove_object(self)
+        pass
+
+    def get_bb(self):
+        return self.x - 25, self.y - 25, self.x + 25, self.y + 25
+
+    def handle_collision(self, group, other):
+        if group == 'chabear:icetea' or group == 'chacat:icetea':
+            game_world.remove_object(self)
+
