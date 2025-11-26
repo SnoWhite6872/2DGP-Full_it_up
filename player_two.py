540bbdefe1719d6d43ae4454322e80e45376a4fb
SnoWhite6872 <hanbong34@gmail.com>
2025-11-26 13:47:32 +0900

    player_one 복사 내용 수정 필요

---
+++ player_two.py
@@ -0,0 +1,14 @@
+from pico2d import *
+import game_framework
+import game_world
+import game_data
+
+class PlayerTwo:
+    def __init__(self, character_index):
+        self.select_char = character_index
+
+    def handle_events(self, event):
+        events_list = get_events()
+        for event in events_list:
+            self.select_char.handle_event(event)
+
