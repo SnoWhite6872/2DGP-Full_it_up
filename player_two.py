3831ccea691916328e893fe7135e3eb451479342
SnoWhite6872 <hanbong34@gmail.com>
2025-11-26 13:57:35 +0900

    입력을 캐릭터 클래스에 맞게 변경

--- player_two.py
+++ player_two.py
@@ -10,5 +10,31 @@ class PlayerTwo:
     def handle_events(self, event):
         events_list = get_events()
         for event in events_list:
-            self.select_char.handle_event(event)
+            if event.type == SDL_KEYDOWN:
+                if event.key == SDLK_UP:
+                    event.key = SDLK_w
+                elif event.key == SDLK_DOWN:
+                    event.key = SDLK_s
+                elif event.key == SDLK_LEFT:
+                    event.key = SDLK_a
+                elif event.key == SDLK_RIGHT:
+                    event.key = SDLK_d
+                elif event.key == SDLK_m:
+                    event.key = SDLK_q
+                elif event.key == SDLK_n:
+                    event.key = SDLK_e
+
+            elif event.type == SDL_KEYUP:
+                if event.key == SDLK_UP:
+                    event.key = SDLK_w
+                elif event.key == SDLK_DOWN:
+                    event.key = SDLK_s
+                elif event.key == SDLK_LEFT:
+                    event.key = SDLK_a
+                elif event.key == SDLK_RIGHT:
+                    event.key = SDLK_d
+                elif event.key == SDLK_m:
+                    event.key = SDLK_q
+                elif event.key == SDLK_n:
+                    event.key = SDLK_e
 
