9abd4589ec23800d70cc6162b8e666a7d5672517
SnoWhite6872 <hanbong34@gmail.com>
2025-12-01 18:09:27 +0900

    게임 데이터 추가

--- game_data.py
+++ game_data.py
@@ -11,5 +11,18 @@ player2_hp = 0
 player2_x = 1360
 player2_y = 700
 
+PIXEL_PER_METER = (1.0 / 0.03)  # 10픽셀 30센치미터
+RUN_SPEED_KMPH = 50.0  # 시속 50킬로미터
+RUN_SPEED_MPM = (RUN_SPEED_KMPH * 1000.0 / 60.0) # 분속
+RUN_SPEED_MPS = (RUN_SPEED_MPM / 60.0)        # 초속
+RUN_SPEED_PPS = (RUN_SPEED_MPS * PIXEL_PER_METER)  #달리기 픽셀 속도
+
+
+TIME_PER_ACTION = 1.0         #1초 액션 당 걸리는 시간
+ACTION_PER_TIME = 1.0 / TIME_PER_ACTION
+FRAMES_PER_IDLE = 5
+FRAMES_PER_TOUCH = 8
+
+
 game_end = 0
 
