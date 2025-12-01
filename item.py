5c09343ec81428c9e29258a545a9aba3a95f4a2a
SnoWhite6872 <hanbong34@gmail.com>
2025-12-01 18:08:06 +0900

    game_data 추가

--- item.py
+++ item.py
@@ -2,6 +2,7 @@ from pico2d import *
 import game_framework
 import game_world
 import random
+import game_data
 
 PIXEL_PER_METER = (1.0 / 0.03)
 
@@ -10,6 +11,7 @@ class Item:
 
     def __init__(self, effect, x = None, y = None):
         self.effect = effect
+        self.y_dir = 1
 
         if self.image_table[effect]:
             self.image = load_image(Item.image_table[effect])
@@ -20,6 +22,7 @@ class Item:
 
         self.x = x
         self.y = y
+        self.set_y = y
         game_world.add_collision_pair('player:item', None, self)
 
 
@@ -32,7 +35,17 @@ class Item:
         #
         # if self.x <0 or self.x > 1480:
         #     game_world.remove_object(self)
-        pass
+
+        if (self.set_y - self.y) <= -8:
+            self.y_dir = -1
+        elif (self.set_y - self.y) >= 8:
+            self.y_dir = 1
+
+        if self.y_dir == 1:
+            self.y += self.y_dir * game_data.FRAMES_PER_TOUCH * game_framework.frame_time
+        elif self.y_dir == -1:
+            self.y += self.y_dir * game_data.FRAMES_PER_TOUCH * game_framework.frame_time
+
 
     def get_bb(self):
         return self.x - 25, self.y - 25, self.x + 25, self.y + 25
