1b54dcb5ddd528dfe06a77056a359055ec706821
SnoWhite6872 <hanbong34@gmail.com>
2025-12-10 19:14:56 +0900

    새로운 키 전달

--- player_one.py
+++ player_one.py
@@ -1,13 +1,11 @@
 from pico2d import *
-import game_framework
-import game_world
-import game_data
+
 
 class PlayerOne:
     def __init__(self, character_index):
         self.select_char = character_index
 
     def handle_event(self, event):
-        if event.key in (SDLK_w, SDLK_s, SDLK_a, SDLK_d, SDLK_q, SDLK_e):
+        if event.key in (SDLK_w, SDLK_s, SDLK_a, SDLK_d, SDLK_q, SDLK_e, SDLK_r):
             self.select_char.handle_event(event)
 
--- player_two.py
+++ player_two.py
@@ -1,14 +1,12 @@
 from pico2d import *
-import game_framework
-import game_world
-import game_data
+
 
 class PlayerTwo:
     def __init__(self, character_index):
         self.select_char = character_index
 
     def handle_event(self, event):
-        if event.key in (SDLK_UP, SDLK_DOWN, SDLK_LEFT, SDLK_RIGHT, SDLK_n, SDLK_m):
+        if event.key in (SDLK_UP, SDLK_DOWN, SDLK_LEFT, SDLK_RIGHT, SDLK_n, SDLK_m, SDLK_b):
             if event.type == SDL_KEYDOWN:
                 if event.key == SDLK_UP:
                     event.key = SDLK_w
@@ -22,6 +20,8 @@ class PlayerTwo:
                     event.key = SDLK_q
                 elif event.key == SDLK_n:
                     event.key = SDLK_e
+                elif event.key == SDLK_b:
+                    event.key = SDLK_r
 
             elif event.type == SDL_KEYUP:
                 if event.key == SDLK_UP:
@@ -36,6 +36,8 @@ class PlayerTwo:
                     event.key = SDLK_q
                 elif event.key == SDLK_n:
                     event.key = SDLK_e
+                elif event.key == SDLK_b:
+                    event.key = SDLK_r
 
             self.select_char.handle_event(event)
 
